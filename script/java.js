dce2d66b95a98d9b56deadf7ead2bbc67ce7ef77
satyamohlan <satyam.ohlan1998@gmail.com>
2018-10-28 21:17:37 +0530

    cac

--- script/java.js
+++ script/java.js
@@ -1,12 +1,13 @@
-document.getElementsByClassName("fa-bars")[0].addEventListener("click",navys);
+document.getElementById("nava").addEventListener("click",navys);
 function navys() {
    if(document.getElementsByClassName("sidebar")[0].style.display ="none"){
     document.getElementsByClassName("sidebar")[0].style.display ="grid";
-    document.getElementById("nava").className="fas fa-times";
-
- }
+    document.getElementsByClassName("fa-bars")[0].className="fas fa-times";
+    document.getElementsByTagName("nav")[0].style.background="black";
+  }
 else if(document.getElementsByClassName("sidebar")[0].style.display ="grid"){
     document.getElementsByClassName("sidebar")[0].style.display ="none";
     document.getElementById("nava").className="fa fa-bars";
 }
+
 }
