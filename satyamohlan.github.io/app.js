d8fe67054d53d6c325d45f08cd234bf135412a07
satyamohlan <35026814+satyamohlan@users.noreply.github.com>
2018-12-07 17:43:55 +0530

    vkm

---
+++ satyamohlan.github.io/app.js
@@ -0,0 +1,21 @@
+document.getElementById("nava").addEventListener("click",navys);
+var show=false;
+function navys() {
+   if(show==false){
+    document.getElementsByClassName("sidebar")[0].style.display ="grid";
+    document.getElementById("nava").className="fas fa-times";
+    document.getElementsByTagName("nav")[0].style.background="black";
+    document.getElementsByClassName("div1")[0].style.position="relative";
+    show=true;
+  }
+  else {
+
+    document.getElementsByClassName("sidebar")[0].style.display ="none";
+    document.getElementById("nava").className="fas fa-bars";
+    document.getElementsByTagName("nav")[0].style.background="transparent";
+    document.getElementsByClassName("div1")[0].style.position="absolute";
+    show=false;
+
+}
+
+}
