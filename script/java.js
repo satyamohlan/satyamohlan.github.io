3c8931e32baa3112ea8e50391680ee5d0dab0bb3
satyamohlan <satyam.ohlan1998@gmail.com>
2018-12-25 22:39:39 +0530

    bar

--- script/java.js
+++ script/java.js
@@ -20,12 +20,23 @@ function navys() {
 }
 
 }
+if($(document).scrollTop()==0){
+  $("nav")[0].style.display="none";
+}
+
+
 $(window).scroll(function(){
-    
-    if($(document).scrollTop()<100){
-      console.log($(document).scrollTop());
-        $("nav")[0].style.top="-"+(60-(($(document).scrollTop()/100)*60))+"px";
-        $("nav")[0].style.opacity=($(document).scrollTop()/100);
-    }
- }  
- );
+
+  if($(document).scrollTop()<110){
+    console.log($(document).scrollTop());
+    $("nav")[0].style.display="block";
+      $("nav")[0].style.top="-"+(60-(($(document).scrollTop()/100)*60))+"px";
+      $("nav")[0].style.opacity=($(document).scrollTop()/100);
+  }
+else{
+  $("nav")[0].style.opacity=1;
+  $("nav")[0].style.display="block";
+  $("nav")[0].style.top=0;
+}
+}  
+);
