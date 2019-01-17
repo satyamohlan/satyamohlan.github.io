4dff87e33f254b2a08ee0bf3a0c4cc23feddab32
satyamohlan <satyam.ohlan1998@gmail.com>
2019-01-17 21:12:34 +0530

    animated

--- script/java.js
+++ script/java.js
@@ -38,3 +38,14 @@ else{
 }
 }  
 );
+var i = 0;
+var txt = 'Lorem ipsum typing effect!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    $(".name h3").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
