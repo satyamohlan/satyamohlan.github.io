a82a7874d73f69ed01896aace24b774f07339727
satyamohlan <satyam.ohlan1998@gmail.com>
2019-10-13 21:52:16 +0530

    ok

---
+++ webdev/selfie/request.js
@@ -0,0 +1,72 @@
+const player = document.getElementById('player');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const captureButton = document.getElementById('capture');
+
+const constraints = {
+  video: true,
+};
+
+captureButton.addEventListener('click', () => {
+  context.drawImage(player, 0, 0, canvas.width, canvas.height);
+  
+  // Stop all video streams.
+ x= player.srcObject.getVideoTracks().forEach(track => track.stop());
+});
+
+navigator.mediaDevices.getUserMedia(constraints)
+  .then((stream) => {
+    // Attach the video stream to the video element and autoplay.
+    player.srcObject = stream;
+  });
+function processImage() {
+    // Replace <Subscription Key> with your valid subscription key.
+    var subscriptionKey = "7741b0a689ee464cab01a73e817e6220";
+
+    var uriBase =
+        "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+    // Request parameters.
+    var params = {
+        "returnFaceId": "true",
+        "returnFaceLandmarks": "false",
+        "returnFaceAttributes":
+            "age,gender,headPose,smile,emotion," +
+            "occlusion,accessories,blur,exposure,noise"
+    };
+
+    // Display the image.
+    var sourceImageUrl =x;
+
+    // Perform the REST API call.
+    $.ajax({
+        url: uriBase + "?" + $.param(params),
+
+        // Request headers.
+        beforeSend: function(xhrObj){
+            xhrObj.setRequestHeader("Content-Type","application/octet-stream");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        },
+
+        type: "POST",
+
+        // Request body.
+        data: '{"url": ' + '"' + sourceImageUrl + '"}',
+    })
+
+    .done(function(data) {
+        // Show formatted JSON on webpage.
+       console.log(data);
+    })
+
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        // Display error message.
+        var errorString = (errorThrown === "") ?
+            "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+        errorString += (jqXHR.responseText === "") ?
+            "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
+                jQuery.parseJSON(jqXHR.responseText).message :
+                    jQuery.parseJSON(jqXHR.responseText).error.message;
+        alert(errorString);
+    });
+};
