952bc20a2964ac5e7d7a17c4e39f8763c01293f1
satyamohlan <satyam.ohlan1998@gmail.com>
2020-06-09 23:02:07 +0530

    ok

--- script/projects.js
+++ script/projects.js
@@ -1,34 +1,17 @@
-var ex=false;
-var exp=  $(".project-list");
-function expand(s){
-if(ex==false){
- exp[s].style.display="none";
- $("div.plus")[s].classList.remove("minus");
+var ex = false;
+var exp = $(".project-list");
 
-      ex=true;
-    }
-else{
-    $(".project-list")[s].style.display="grid";
+function expand(s) {
+  if (ex == false) {
+    exp[s].style.display = "none";
+    $("div.plus")[s].classList.remove("minus");
+
+    ex = true;
+  } else {
+    $(".project-list")[s].style.display = "inline-block";
     $("div.plus")[s].classList.add("minus");
 
 
-    ex=false;
-}}
-$(window).on('load', function(){
-    $.instagramFeed({
-      'username': 'Ohlansatyam',
-      'container': "#instagram-feed1",    
-      'display_profile': true,
-      'display_biography': true,
-      'display_gallery': true,
-      'get_raw_json': true,
-
-      'callback': null,
-      'styling': false,
-      'items': 80,
-      'items_per_row': 1,
-      'margin': 1 
-  
-    })});
-    
-  
+    ex = false;
+  }
+}
