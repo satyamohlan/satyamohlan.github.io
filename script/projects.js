56b76160788a0f2dcdf49dcdb53a57b67aec3581
satyamohlan <satyam.ohlan1998@gmail.com>
2020-07-01 13:45:50 +0530

    improvements to home page,project page and nav bar

--- script/projects.js
+++ script/projects.js
@@ -1,13 +1,17 @@
 var ex = false;
 var exp = $(".project-list");
+var button = $('button.close');
 
 function expand(s) {
   if (ex == false) {
+    button[s].style.borderColor = 'rgb(8, 100, 161)'
     exp[s].style.display = "none";
     $("div.plus")[s].classList.remove("minus");
 
     ex = true;
   } else {
+    button[s].style.borderColor = '#64B5F6'
+
     $(".project-list")[s].style.display = "flex";
     $("div.plus")[s].classList.add("minus");
 
@@ -22,7 +26,7 @@ if ($(document).scrollTop() == 0) {
 }
 
 $(window).scroll(function () {
-  console.log(Math.round($(document).scrollTop() / 100) / 10);
+
   if ($(document).scrollTop() < 200) {
     $("nav")[0].style.backgroundColor = "rgba(0, 0, 0," + (Math.round($(document).scrollTop() / 100) / 12) + ")";
   } else {
